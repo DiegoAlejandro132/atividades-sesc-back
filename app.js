96da593f49bc32635a2eba818406d53708bf4578
Terceros <diego.terceros@ikhon.com.br>
2025-07-08 19:26:32 -0400

    criacao do projeto

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Servidor está rodando. A porta é: "+ PORT);
+    else 
+        console.log("Houve um erro e o servidor não pôde iniciar", error);
+    }
+);
